8dc147d2aa3577c6513a73bebfa543f5fb1a8d62
Ironfist2004 <43863655+Ironfist2004@users.noreply.github.com>
2019-12-01 13:15:35 -0500

    Update RobotHardware.java

--- teamcode/RobotHardware.java
+++ teamcode/RobotHardware.java
@@ -1,6 +1,6 @@
 /*
- * The Robot Hardware code for the Destriers
- * Created by Team Member Shourya Bansal
+ * The Robot Hardware code for the Livingston-Arad
+ * Created by Team Member Aryan Bansal
  */
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -13,26 +13,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class RobotHardware {
     //Declaring the specific hardware for our robot
     //The DC Motors:
-    private DcMotor frontRightMotor;
-    private DcMotor frontLeftMotor;
-    private DcMotor backRightMotor;
-    private DcMotor backLeftMotor;
-  
-    private DcMotor clawMotor;
-    //Add more DC Motors when you use them
-
+    private DcMotor middle_drive;
+    private DcMotor leftFrontMotor;
+    private DcMotor rightFrontMotor;
+/*
     //The Servos
-    private Servo clawServo;
+    private Servo liftClawServo;
     private Servo servoTwo;
-
+    private Servo servoThree;
+    private Servo servoFour;
+*/
     //The Color Sensors
     private ColorSensor colorSensor;
-
-    //The Gyro Sensor
-    private GyroSensor gyroSensor;
-
-    //Distance Sensor
-    private DistanceSensor distanceSensor;
 
     private static final double SERVO_INIT_POS = 0.5;//This is the initial position of a servo and what you will send it back to
     private static final double SERVO_OPEN_POS = 45;
@@ -44,54 +36,46 @@ public class RobotHardware {
         hardwareMap = aHardwareMap;
 
         //Initializing Motors
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "Front Left Motor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "Front Right Motor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "Back Left Motor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "Back Right Motor");
-        clawMotor = hardwareMap.get(DcMotor.class, "Claw Motor");
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        middle_drive = hardwareMap.get(DcMotor.class, "middle_drive");
 
         //Set Directions
-        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-        backLeftMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
-        backRightMotor.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
-
-        clawMotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE); 
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        middle_drive.setDirection(DcMotor.Direction.FORWARD);
 
 
         //Set Motor Power to Zero
-        frontLeftMotor.setPower(0);
-        frontRightMotor.setPower(0);
-        clawMotor.setPower(0);
+        leftFrontMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        middle_drive.setPower(0);
 
         //Run with Encoders. If we don't use Encoders, change "RUN_USING_ENCODERS" to "RUN_WITHOUT_ENCODERS"
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        clawMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        middle_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        
         //Brakes the Motors when the power is at Zero
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        clawMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        middle_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+/*
         //Define and Initialize Servos
         clawServo = hardwareMap.get(Servo.class, "<Servo One Name>");
         servoTwo = hardwareMap.get(Servo.class, "<Servo Two Name>");
         clawServo.setPosition(SERVO_INIT_POS);
         servoTwo.setPosition(SERVO_INIT_POS);
-
+*/
         //Define a Color Sensor
         //Used https://ftc-tricks.com/overview-color-sensor/ to initialize and use Color Sensor
-        colorSensor = hardwareMap.get(ColorSensor.class, "Lego Detector");
-
+        colorSensor = hardwareMap.get(ColorSensor.class, "Block Detector");
+/*
         //Define Gyro Sensor
         gyroSensor = hardwareMap.get(GyroSensor.class, "Gyro Sensor");
-
         //Define Distance Sensor
         distanceSensor = hardwareMap.get(DistanceSensor.class, "Distance Sensor");
+        */
     }
+   
 }
