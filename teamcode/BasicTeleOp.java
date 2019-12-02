fe6f105e85402b55d321a6c6c85d1d69d92bce64
Mastermind497 <39387439+Mastermind497@users.noreply.github.com>
2019-12-02 08:55:34 -0500

    Update BasicTeleOp.java

--- teamcode/BasicTeleOp.java
+++ teamcode/BasicTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Teleop;
+package org.firstinspires.ftc.teamcode;
 
 /*
  * The Driver-Controlled code for FireStorm Robotics Team
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.teamcode.RobotHardware;
+import org.firstinspires.ftc.teamcode.*;
 
 @TeleOp(name="Basic TeleOp", group="TeleOp")
 
