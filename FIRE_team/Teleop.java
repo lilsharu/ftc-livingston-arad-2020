52e0e9d49fa2bda064bab56ed618773468374c22
Marvin4213 <52053423+Marvin4213@users.noreply.github.com>
2019-12-06 15:20:59 -0500

    Update Teleop.java

--- FIRE_team/Teleop.java
+++ FIRE_team/Teleop.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 /* Copyright (c) 2017 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -27,8 +29,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -48,6 +48,8 @@ public class Teleop extends LinearOpMode {
     // Declare OpMode members.
 
     Hardware robot = new Hardware();
+
+    private Robot robotObj;
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftSide;
     private DcMotor griper;
@@ -61,6 +63,8 @@ public class Teleop extends LinearOpMode {
     private final double minimalHight = 52;
     private final double level_height= 103;
     private boolean IsPressed = false;
+    private double deadZone = 0.1;
+
 
 
 
@@ -80,6 +84,15 @@ public class Teleop extends LinearOpMode {
 
 
 
+        //Varibles for joystick position
+        double G1rightStickY = -gamepad1.right_stick_y;
+        double G1leftStickY = -gamepad1.left_stick_y;
+        double G1rightStickX = -gamepad1.right_stick_x;
+        double G1leftStickX = -gamepad1.left_stick_x;
+
+
+
+
         // Setup a variable for each drive wheel to save power level for telemetry
         double leftPower;
         double rightPower;
@@ -89,7 +102,7 @@ public class Teleop extends LinearOpMode {
         boolean rollerGriperIsOpen = true;
         boolean foundation_griperIsOpen = true;
         int level = 0;
-        int range = 14;
+        int range = 20;
 
 
 
@@ -105,7 +118,7 @@ public class Teleop extends LinearOpMode {
             telemetry.addData("range", String.format("%.01f mm", lift_ctrl.getDistance(DistanceUnit.MM)));
             double trigger_button = gamepad2.right_trigger;
 //
-          telemetry.addData( "trigger_button",(trigger_button));
+            telemetry.addData( "trigger_button",(trigger_button));
 
 //
 //                if( lift_ctrl.getDistance(DistanceUnit.MM)<lift1.High() && gamepad1.a ){
@@ -119,49 +132,46 @@ public class Teleop extends LinearOpMode {
 
 
 //Would be start of lift method???
-            if(gamepad2.dpad_up && level < 7 && !IsPressed) { //Lift
-                level++;
-                IsPressed = true;
-            }
-            else
-            {
-                IsPressed=false;
-            }
+            //Change to is pressed?
+            if (gamepad2.dpad_down || gamepad2.dpad_up || gamepad2.dpad_left || gamepad2.dpad_right) { //Should stop lift from moving without dpad being pressed
+                if (gamepad2.dpad_up && level < 6 && !IsPressed) { //Lift
+                    level++;
+                    IsPressed = true;
+                } else {
+                    IsPressed = false;
+                }
 
-            if(gamepad2.dpad_down && level > 0 && !IsPressed)
-            {
-                level--;
-                IsPressed = true;
-            }else {
-                IsPressed=false;
-            }
-
-
-            if(gamepad2.b)
-            {
-                level = 0;
-            }
+                if (gamepad2.dpad_down && level > 0 && !IsPressed) { //!IsPressed part stops someone from spamming it down
+                    level--;
+                    IsPressed = true;
+                } else {
+                    IsPressed = false;
+                }
 
 
-            double error = Math.abs(lift_ctrl.getDistance(DistanceUnit.MM) - (minimalHight + level_height * level));
-            double sample = lift_ctrl.getDistance(DistanceUnit.MM) - (minimalHight + level_height * level);
+                if (gamepad2.b) {
+                    level = 0;
+                }
+
+                //Math to move to lift
+                double error = Math.abs(lift_ctrl.getDistance(DistanceUnit.MM) - (minimalHight + level_height * level));
+                double sample = lift_ctrl.getDistance(DistanceUnit.MM) - (minimalHight + level_height * level);
 //            && !(error < range)
 
+                //Either add condition Ispressed == true or make method
+                if (error > range) { //Shouldn't be runable when dpad hasn't been pressed
+                    //Stops lift from raising on int if sensor is disconnected
+                    if (sample >= 5000) {
+                        telemetry.addLine("Distance sensor doesn't work");
+                    } else if (sample < 0) {
+                        lift_test.setPower(0.8);
+                    } else if (sample > 0) {
+                        lift_test.setPower(-0.8);
+                    }
 
-            if(error > range)
-            {
-                if(sample < 0)
-                {
-                    lift_test.setPower(0.8);
+                } else {
+                    lift_test.setPower(0);
                 }
-                else if(sample > 0)
-                {
-                    lift_test.setPower(-0.8);
-                }
-            }
-            else
-            {
-                lift_test.setPower(0);
             }
 //Would be end of lift method???
 
@@ -176,6 +186,7 @@ public class Teleop extends LinearOpMode {
 
             rightPower = leftPower  = -gamepad1.left_stick_y ;
             middlePower = -gamepad1.left_stick_x ;
+            //finalTeleopStrafe(G1rightStickY,G1leftStickY, G1rightStickX, G1leftStickX);
 
 
             if(gamepad1.left_bumper)
@@ -261,26 +272,65 @@ public class Teleop extends LinearOpMode {
             telemetry.update();
         }
     }
+
     int maxPos=300;//Change later
     public void ControlLift(double power, int num){
         int intpos = lift_test.getCurrentPosition();
         //Checks if is at max height and num is postive
-        if (intpos >= (maxPos-num) && num>0){
+        if (intpos >= (maxPos-num) && num>0){ //If lift goes higher will go out of bounds
             telemetry.addLine("Lift won't go higher");
-        }else if (intpos <= 0 && num<0){
+        }else if (intpos <= 0 && num<0){//If lift goes lower it will go out of bounds
             telemetry.addLine("Lift won't go lower");
+        }else {
+            telemetry.addData("init pos:", intpos);
+            //lift_test.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            lift_test.setTargetPosition(intpos + num);
+            lift_test.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lift_test.setPower(power);
+            while (lift_test.isBusy()) {
+                //Waits for lift
+            }
+            lift_test.setPower(0);
+            lift_test.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-        telemetry.addData("init pos:", intpos);
-        //lift_test.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift_test.setTargetPosition(intpos+num);
-        lift_test.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lift_test.setPower(power);
-        while (lift_test.isBusy()){
-            //Waits for lift
-        }
-        lift_test.setPower(0);
-        lift_test.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
     }
+
+    public void finalTeleopStrafe(double G1leftStickY, double G1rightStickX, double G1leftStickX){
+        if (Math.abs(G1leftStickY) < deadZone && Math.abs(G1leftStickX) < deadZone && Math.abs(G1rightStickX) < deadZone) {
+            robotObj.turnOff();
+        }
+        else if (Math.abs(G1leftStickY)< deadZone && Math.abs(G1leftStickX)< deadZone && Math.abs(G1rightStickX) >= deadZone) {
+            if (G1rightStickX > 0) {
+                robotObj.turn(G1rightStickX, "r");
+            }
+            else {
+                robotObj.turn(-(G1rightStickX), "l");
+            }
+        }
+        else if (((Math.abs(G1leftStickY) >= deadZone) || Math.abs(G1leftStickX) >= deadZone) && Math.abs(G1rightStickX) < deadZone) {
+            robotObj.setForwardPower(G1leftStickY);
+            robotObj.strafe(G1leftStickX);
+        }
+        else {
+            robotObj.setRightPower(rightPower(G1leftStickY, G1rightStickX));
+            robotObj.setLeftPower(leftPower(G1leftStickY, G1rightStickX));
+            robotObj.strafe(G1leftStickX);
+        }
+
+    }
+
+    public double rightPower(double leftY, double rightY){
+        return 0.5 * leftY + 0.5 * rightY;
+    }
+
+    public double leftPower(double leftY, double rightY) {
+        return 0.5 * leftY - 0.5 * rightY;
+    }
+
+
 
     /*public void SetUpEncodersForDistance(int distance){
         //Reset encoder values
