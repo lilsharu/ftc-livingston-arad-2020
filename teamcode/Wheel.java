f95f8a06dfe14fc2dd29bded862e756d0a916b3c
Marvin4213 <52053423+Marvin4213@users.noreply.github.com>
2019-12-21 08:12:01 -0800

    Update Wheel.java

--- teamcode/Wheel.java
+++ teamcode/Wheel.java
@@ -1,43 +1,87 @@
-package org.firstinspires.ftc.teamcode.Auton;
-
+package org.firstinspires.ftc.teamcode;
+import org.firstinspires.ftc.teamcode.*;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
+/*
+ * The Wheel Class implemented in the FIRE Team Code
+ * Designed by Shourya Bansal and Ally Mintz
+ */
+//package org.firstinspires.ftc.teamcode;
+
 public class Wheel {
-    private DcMotor theMotor;
-    private double radius;
+    private double radius; //This is in millimeters
+    private String position; //The Robot Position (eg Front Left, Middle, etc)
+    private String type; //The type of wheel (that is, omni, mecanum, tank, or any other type)
+    private String orgUn;
     private int ticks;
-    private String position;
-
-    public Wheel(DcMotor m, double r, int ticks, String pos){
-        this.theMotor = m;
-        this.radius = r;
+    //Constructors for a Wheel Object
+    public Wheel(double radius, int ticks, String position, String type, String unit) {
+        //Sets instance variables equal to User Arguments
+        this.radius = radius;
         this.ticks = ticks;
-        this.position = pos;
+        this.position = position;
+        this.type = type;
+        this.orgUn = unit;
     }
 
-    public Wheel(DcMotor m){
-        this.theMotor = m;
+    public Wheel(double radius, int ticks, String type, String unit) {
+        //Sets instance variables equal to User arguments and leaves position unassigned
+        this(radius, ticks, "Unassigned", type, unit);
     }
 
+    public Wheel(double radius, int ticks) {
+        //Sets instance variables equal to User arguments and leaves position unassigned
+        this(radius, ticks, "Unassigned", "Unknown", "mm");
+    }
 
-
+    //Getters and Setters for Instance Variables to get values and to change values
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public double getNumOfRots(double distance, String unit){
+        //converts distance to inches
+        double inchDist = distance;
+        return distance/getDPR();
+    }
     public double getDPR(){
         return radius*2*Math.PI;
     }
 
     public double getNumOfRot(double distance){
-        return distance/radius;
+        return distance/getDPR();
     }
 
-    public int getNumOfTicks(){
-        double dpr= getDPR();
-        return (int) dpr*ticks; //Add in a rounding method
+    public int getNumOfTicks(double dist){
+        double nor= getNumOfRot(dist);
+        return (int) Math.round(nor*ticks);
     }
 
+    public int getDistance(int pos){
+        int rotations =  Math.round(pos/ticks);
+        int dpr = (int) getDPR();
+        return dpr*rotations;
+
+    }
+
+    //What will print when we convert it to a String:
     @Override
     public String toString(){
-        return "Position: +"+position+"; Radius: "+radius+"; DPR: "+getDPR()+" ticks: "+ticks;
+        return "Radius: " + radius + ", Position on Robot: " + position + ", Type of Wheel: " + type;
     }
-
-
 }
