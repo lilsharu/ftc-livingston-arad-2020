adbbe84137da275d03681b4026ce32867182cc45
Shourya Bansal <shouryabansal2012@gmail.com>
2020-02-09 17:04:04 -0500

    Fixed Errors

--- FTC FIRE Team/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FIRE_team/Hardware.java
+++ FTC FIRE Team/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FIRE_team/Hardware.java
@@ -4,32 +4,20 @@ package org.firstinspires.ftc.teamcode.FIRE_team;
  */
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 
-
-import org.firstinspires.ftc.robotcore.external.Func;
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-
-import java.util.Locale;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
 public class Hardware{
     /** Declare  the parts and verbals   */
 
-    BNO055IMU imu;
+    public BNO055IMU imu;
 
     public DcMotor  middleDrive = null;
     public DcMotor liftMotor = null;
